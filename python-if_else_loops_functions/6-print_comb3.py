a7327d45c3221660c94b7bc914935e4ec988c02d
PJ <63235746+pilot-0077@users.noreply.github.com>
2025-12-27 14:31:56 +0400

    Fix print statement formatting in 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i + 1, 10):
-        print("{}{}".format(i, j), end=", " if not (i == 8 and j == 9) else "\n")
+        print("{}{}".format(i, j), end=", " if not (i == 8 and j == 9) else "\n"        )
