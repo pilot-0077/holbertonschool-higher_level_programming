0627aea2d9670801d34abccf45da9f181eee33ae
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-01-12 16:24:28 +0400

    Add files via upload

---
+++ python-more_classes/101-nqueens.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_solution(board):
+    solution = []
+    for row in range(len(board)):
+        solution.append([row, board[row]])
+    print(solution)
+
+
+def is_safe(board, row, col):
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+
+def solve_nqueens(board, row):
+    n = len(board)
+    if row == n:
+        print_solution(board)
+        return
+
+    for col in range(n):
+        if is_safe(board, row, col):
+            board[row] = col
+            solve_nqueens(board, row + 1)
+            board[row] = -1
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * n
+    solve_nqueens(board, 0)
+
+
+if __name__ == "__main__":
+    main()
