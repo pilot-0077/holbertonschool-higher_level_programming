bc893003eea27024fb3efa44ce978b7eebfbbda6
pilot-0077 <parvin.jumshudlu@gmail.com>
2026-01-12 05:53:24 +0000

    Move 1-element_at.py into python-data_structures

--- 1-element_at.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-def element_at(my_list, idx):
-    """Return element at index idx or None if index is invalid"""
-    if idx < 0 or idx >= len(my_list):
-        return None
-    return my_list[idx]
--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    if idx >= len(my_list):
+    """Return element at index idx or None if index is invalid"""
+    if idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
