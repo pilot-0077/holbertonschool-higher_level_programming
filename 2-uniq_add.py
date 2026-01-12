a9f14fb34e6a55099946de32ec1aafa2773ba532
PJ <63235746+pilot-0077@users.noreply.github.com>
2026-01-12 09:28:36 +0400

    Delete 2-uniq_add.py

--- 2-uniq_add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def uniq_add(my_list=[]):
-    elements = []
-    sum = 0
-    for element in my_list:
-        if element not in elements:
-            elements.append(element)
-            sum += element
-    return sum
